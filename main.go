6b7bedcb3dd71dc46f677db97e628cc8d1a09052
Fabio Pereira <fabiuhp@msn.com>
2024-07-12 18:10:51 -0300

    estrutura basica

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Erro carregando arquivo .env")
+	}
+	println(os.Getenv("TEST"))
+}
