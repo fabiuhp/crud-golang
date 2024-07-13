790d272ce4b0238adf756549cd5785f025221258
Fabio Pereira <fabiuhp@msn.com>
2024-07-12 22:40:54 -0300

    user req e res

--- src/controller/usecases/createUser.go
+++ src/controller/usecases/createUser.go
@@ -1,11 +1,22 @@
 package usecases
 
 import (
+	"fmt"
+
 	"github.com/fabiuhp/crud-golang/src/configurations/rest_err"
+	"github.com/fabiuhp/crud-golang/src/model/req"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Voce chamou a rota errada")
-	c.JSON(err.Code, err)
+	var userReq req.UserRequest
+
+	if err := c.ShouldBindJSON(&userReq); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("Existem campos incorretos, error=%s", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userReq)
 }
