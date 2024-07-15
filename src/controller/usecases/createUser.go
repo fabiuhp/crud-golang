a697d8daf8ca14d92815698aefd4a36b00f40757
Fabio Pereira <fabiuhp@msn.com>
2024-07-15 09:54:11 -0300

    alterar logs

--- src/controller/usecases/createUser.go
+++ src/controller/usecases/createUser.go
@@ -2,19 +2,21 @@ package usecases
 
 import (
 	"fmt"
-	"log"
+	"net/http"
 
+	"github.com/fabiuhp/crud-golang/src/configurations/logger"
 	"github.com/fabiuhp/crud-golang/src/configurations/validations"
 	"github.com/fabiuhp/crud-golang/src/model/req"
+	"github.com/fabiuhp/crud-golang/src/model/res"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	log.Println("Iniciando controller: CreateUser")
+	logger.Info("Iniciando controller: CreateUser")
 	var userReq req.UserRequest
 
 	if err := c.ShouldBindJSON(&userReq); err != nil {
-		log.Printf("Erro tentando marshal o objeto, error=%s\n", err.Error())
+		logger.Error("Erro tentando marshal o objeto", err)
 		errRest := validations.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
@@ -22,4 +24,12 @@ func CreateUser(c *gin.Context) {
 	}
 
 	fmt.Println(userReq)
+	res := res.UserResponse{
+		ID:    "teste",
+		Email: userReq.Email,
+		Name:  userReq.Name,
+		Age:   userReq.Age,
+	}
+	logger.Info("Usuario criado com sucesso")
+	c.JSON(http.StatusOK, res)
 }
