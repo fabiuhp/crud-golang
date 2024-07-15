62e4e0f397f488d72afff32700332924248d07c4
Fabio Pereira <fabiuhp@msn.com>
2024-07-15 09:08:55 -0300

    validacao 2

--- src/controller/usecases/createUser.go
+++ src/controller/usecases/createUser.go
@@ -2,19 +2,22 @@ package usecases
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/fabiuhp/crud-golang/src/configurations/rest_err"
+	"github.com/fabiuhp/crud-golang/src/configurations/validations"
 	"github.com/fabiuhp/crud-golang/src/model/req"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
+	log.Println("Iniciando controller: CreateUser")
 	var userReq req.UserRequest
 
 	if err := c.ShouldBindJSON(&userReq); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("Existem campos incorretos, error=%s", err.Error()))
-		c.JSON(restErr.Code, restErr)
+		log.Printf("Erro tentando marshal o objeto, error=%s\n", err.Error())
+		errRest := validations.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 
